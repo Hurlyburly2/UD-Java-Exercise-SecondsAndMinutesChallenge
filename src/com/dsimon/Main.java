85609f31c25537e867529764648a376dbf7822bb
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-20 00:50:02 -0400

    exercise complete

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -3,6 +3,9 @@ package com.dsimon;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(DurationString.getDurationString(120, 10));
+        System.out.println(DurationString.getDurationString(1000));
+        System.out.println(DurationString.getDurationString(-30, 10));
+        System.out.println(DurationString.getDurationString(-200));
     }
 }
